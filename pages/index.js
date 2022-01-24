7d644797b99cf269d2b0dbb24f02f778251ce4b0
Kgaogel09 <kgaogelo@swipeix.com>
2022-01-25 00:13:09 +0200

    refactor(events): rendered all events

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,9 @@ import { getFeaturedEvents } from "../dummy-data";
 
 function HomePage() {
   const featuredEvents = getFeaturedEvents();
-  console.log(featuredEvents);
 
   return (
     <div>
-      <h1>Home Page!</h1>
       <EventList items={featuredEvents} />
     </div>
   );
