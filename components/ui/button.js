fbeb903f9021fd9194b7ea8e235e536122e68be2
Kgaogel09 <kgaogelo@swipeix.com>
2022-01-25 09:00:02 +0200

    feat(events): created events search

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import classes from "./button.module.css";
 
-function Button({ link, children }) {
-  return (
-    <Link href={link}>
-      <a className={classes.btn}>{children}</a>
-    </Link>
-  );
+function Button({ link, children, onClick }) {
+  if (link) {
+    return (
+      <Link href={link}>
+        <a className={classes.btn}>{children}</a>
+      </Link>
+    );
+  }
+
+  return <button onClick={onClick}>{children}</button>;
 }
 
 export default Button;
