e5fd77b95d5f3e1b41c2727b95e3117e21b96ae3
Kgaogel09 <kgaogelo@swipeix.com>
2022-01-25 09:23:00 +0200

    delete(events): renamed events search

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import EventList from "../../components/events/event-list";
-import EventsSearch from "../../components/events/events-search";
+import EventsSearch from "../../components/events/event-search";
 import { getAllEvents } from "../../dummy-data";
 
 function EventsPage() {
