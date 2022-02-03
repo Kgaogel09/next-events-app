24249fb14c906be0d3b7813b2e1ecfebbc09f0ba
Kgaogel09 <kgaogelo@swipeix.com>
2022-02-03 11:30:40 +0200

    refactor(api): error handling

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,17 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://thorn:IRI0MuXdi2kEBUaE@cluster0.naxcy.mongodb.net/events?retryWrites=true&w=majority"
+  );
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection("newsletter").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -8,15 +20,24 @@ async function handler(req, res) {
       res.status(422).json({ message: "Invalid email address" });
     }
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://thorn:IRI0MuXdi2kEBUaE@cluster0.naxcy.mongodb.net/events?retryWrites=true&w=majority"
-    );
-    const db = client.db();
+    let client;
 
-    await db.collection("newsletter").insertOne({ email: userEmail });
-    client.close(); //to disconnect
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "connection to database failed" });
+      return;
+    }
 
-    res.status(200).json({ message: "This is it!" });
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close(); //to disconnect
+    } catch (error) {
+      res.status(500).json({ message: "connection to database failed" });
+      return;
+    }
+
+    res.status(200).json({ message: "Signed up!" });
   }
 }
 export default handler;
