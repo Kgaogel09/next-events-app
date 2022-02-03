39a65bc88b0f6ef8fd1bc7e9df7ed265b2918dcc
Kgaogel09 <kgaogelo@swipeix.com>
2022-02-03 10:27:07 +0200

    refactor(api): run MongoDB queries from inside api routes

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -6,7 +8,14 @@ function handler(req, res) {
       res.status(422).json({ message: "Invalid email address" });
     }
 
-    console.log(userEmail);
+    const client = await MongoClient.connect(
+      "mongodb+srv://thorn:IRI0MuXdi2kEBUaE@cluster0.naxcy.mongodb.net/newsletter?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    await db.collection("emails").insertOne({ email: userEmail });
+    client.close(); //to disconnect
+
     res.status(200).json({ message: "This is it!" });
   }
 }
