0f6e17cec2acaeb004fbd4e71c95c9d654198068
Kgaogel09 <kgaogelo@swipeix.com>
2022-02-03 17:15:50 +0200

    refactor(app): created notification context

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,21 @@
 import Head from "next/head";
 import Layout from "../components/layout/layout";
+import NotificationContext from "../store/notification-context";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContext>
+      <Layout>
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContext>
   );
 }
 
