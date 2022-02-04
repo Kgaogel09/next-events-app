ee41591e3498c16f1ea35ba91d2aa654b0c5924d
Kgaogel09 <kgaogelo@swipeix.com>
2022-02-04 09:49:09 +0200

    refactor(layout): pass context data in components

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,11 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import MainNav from "./main-nav";
+import Notification from "../notification/notification";
+import NotificationContext from "../../store/notification-context";
 
 function Layout({ children }) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
   return (
     <Fragment>
       <MainNav />
       <main>{children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </Fragment>
   );
 }
